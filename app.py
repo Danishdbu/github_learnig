c37a1480bc9e147df54b27ac39c0f55a468e657e
Danishdbu <mohddanishdbu@gmail.com>
2024-04-03 16:22:50 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title('campusx')
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image('unknown.jpg')
+with col1:
+    st.write("""A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.""")
