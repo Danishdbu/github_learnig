017ad3fdc6a952caf59e367a20d07d6aecd88881
Danishdbu <mohddanishdbu@gmail.com>
2024-04-03 16:33:43 +0530

    add courses to home page

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ with col1:
     st.image('unknown.jpg')
 with col1:
     st.write("""A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.""")
+st.header('Courses offer')
+st.subheader('Data Science')
+st.subheader('Data Analysis')
+st.subheader('Ai')
