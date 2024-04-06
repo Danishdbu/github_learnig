6ac40066f78d517458bc088944990293c489bf9d
Danishdbu <mohddanishdbu@gmail.com>
2024-04-06 16:40:54 +0530

    change header image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('campusx')
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image('unknown.jpg')
+    st.image('unknown2.PNG')
 with col1:
     st.write("""A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.""")
 st.header('Courses offer')
